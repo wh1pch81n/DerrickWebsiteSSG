0a77b63d6dc8ed34c2e5106918eb1ff59d58e2c8
wh1pch81n <wh1pch81n@gmail.com>
2013-12-21 18:37:46 -0800

    Created the model

---
+++ ScriptGen/DHSlideModel.h
@@ -0,0 +1,13 @@
+//
+//  DHSlideModel.h
+//  ScriptGen
+//
+//  Created by Derrick Ho on 12/21/13.
+//  Copyright (c) 2013 Derrick Ho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DHSlideModel : NSObject
+@property (strong, atomic) NSString *code, *header, *comment;
+@end
---
+++ ScriptGen/DHSlideModel.m
@@ -0,0 +1,13 @@
+//
+//  DHSlideModel.m
+//  ScriptGen
+//
+//  Created by Derrick Ho on 12/21/13.
+//  Copyright (c) 2013 Derrick Ho. All rights reserved.
+//
+
+#import "DHSlideModel.h"
+
+@implementation DHSlideModel
+
+@end
