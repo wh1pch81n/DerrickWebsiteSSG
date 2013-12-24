a3a5f2d910975caa02ce581141fa573d668a21a4
wh1pch81n <wh1pch81n@gmail.com>
2013-12-24 15:50:50 -0800

    Figured out how to save string to file Getting contents of array conroller

--- ScriptGen/DHDocument.m
+++ ScriptGen/DHDocument.m
@@ -13,50 +13,75 @@
 
 - (id)init
 {
-    self = [super init];
-    if (self) {
-        // Add your subclass-specific initialization here.
-    }
-    return self;
+	self = [super init];
+	if (self) {
+		// Add your subclass-specific initialization here.
+	}
+	return self;
 }
 
 - (NSString *)windowNibName
 {
-    // Override returning the nib file name of the document
-    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
-    return @"DHDocument";
+	// Override returning the nib file name of the document
+	// If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
+	return @"DHDocument";
 }
 
 - (void)windowControllerDidLoadNib:(NSWindowController *)aController
 {
-    [super windowControllerDidLoadNib:aController];
-    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+	[super windowControllerDidLoadNib:aController];
+	// Add any code here that needs to be executed once the windowController has loaded the document's window.
 }
 
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    if (outError) {
-        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:nil];
-    }
-    return nil;
+	// Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
+	// You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
+	//    if (outError) {
+	//        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:nil];
+	//    }
+	//
+	//
+	//
+	//    return nil;
+	
+	NSData *data;
+	//[self setMString:[self.textView textStorage]]; // Synchronize data model with the text storage
+	NSMutableDictionary *dict = [[NSDictionary dictionaryWithObject:NSPlainTextDocumentType
+																													forKey:NSDocumentTypeDocumentAttribute] mutableCopy];
+	//[self.textView breakUndoCoalescing];
+	NSString *temp = @"hello World";
+	//NSLog(@"%@", self.SlideArrayController );
+	for (DHSlideModel *m in self.SlideArrayController.content) {// use this to draw out the contents of the array controller
+    NSLog(@"%@", m);
+		NSLog(@"%@", m.code);
+		NSLog(@"%@", m.header);
+		NSLog(@"%@", m.comment);
+	}
+	data = [temp  dataUsingEncoding:NSASCIIStringEncoding];
+	//data = [self.mString dataFromRange:NSMakeRange(0, [self.mString length])
+//									documentAttributes:dict error:outError];
+	if (!data && outError) {
+		*outError = [NSError errorWithDomain:NSCocoaErrorDomain
+																		code:NSFileWriteUnknownError userInfo:nil];
+	}
+	return data;
 }
 
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-    // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-    // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-    if (outError) {
-        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:nil];
-    }
-    return YES;
+	// Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
+	// You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
+	// If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
+	if (outError) {
+		*outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:nil];
+	}
+	return YES;
 }
 
 + (BOOL)autosavesInPlace
 {
-    return YES;
+	return YES;
 }
 
 - (BOOL)control:(NSControl *)control textView:(NSTextView *)textView doCommandBySelector:(SEL)commandSelector {
