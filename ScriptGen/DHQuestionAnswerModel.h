1b4159a77ae3f9b55cd843e7becba37c125bfc0c
wh1pch81n <wh1pch81n@gmail.com>
2013-12-27 08:50:04 -0800

    Create Model Object.  changed names to match the IB Binded Array controller together with the Table and the

--- ScriptGen/DHQuestionAnswerModel.h
+++ ScriptGen/DHQuestionAnswerModel.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface DHQuestionAnswerModel : NSObject
-@property (strong, atomic) NSString *question, *answer;
+@property (strong, atomic) NSString *mQuestion, *mAnswer;
 @end
