1bc8f78c296d5cb2fa30c303b620b0d5c7550765
wh1pch81n <wh1pch81n@gmail.com>
2013-12-27 08:19:14 -0800

    Add Another Array Controller Create Q&A model

---
+++ ScriptGen/DHQuestionAnswerModel.h
@@ -0,0 +1,13 @@
+//
+//  DHQuestionAnswerModel.h
+//  ScriptGen
+//
+//  Created by Derrick Ho on 12/27/13.
+//  Copyright (c) 2013 Derrick Ho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DHQuestionAnswerModel : NSObject
+@property (strong, atomic) NSString *question, *answer;
+@end
---
+++ ScriptGen/DHQuestionAnswerModel.m
@@ -0,0 +1,13 @@
+//
+//  DHQuestionAnswerModel.m
+//  ScriptGen
+//
+//  Created by Derrick Ho on 12/27/13.
+//  Copyright (c) 2013 Derrick Ho. All rights reserved.
+//
+
+#import "DHQuestionAnswerModel.h"
+
+@implementation DHQuestionAnswerModel
+
+@end
